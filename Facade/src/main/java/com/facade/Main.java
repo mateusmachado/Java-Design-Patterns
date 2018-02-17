1a4692dc880205aa548821d09e60f1ce7cf3d28f
Mateus <mmachado.csp@gmail.com>
2018-02-17 20:53:53 -0200

    created program entry point

---
+++ Facade/src/main/java/com/facade/Main.java
@@ -0,0 +1,10 @@
+package com.facade;
+
+public class Main {
+    public static void main(String[] args){
+        DwarvenGoldmineFacade facade = new DwarvenGoldmineFacade();
+        facade.startNewDay();
+        facade.digOutGold();
+        facade.endDay();
+    }
+}
