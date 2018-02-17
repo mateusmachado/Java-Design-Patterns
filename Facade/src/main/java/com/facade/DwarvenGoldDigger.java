ca7fca664e9c3915283b0fe8555961d2afbded49
Mateus <mmachado.csp@gmail.com>
2018-02-17 21:25:15 -0200

    create DwarvenGoldDigger - is one of the goldmine subsystems

---
+++ Facade/src/main/java/com/facade/DwarvenGoldDigger.java
@@ -0,0 +1,18 @@
+package com.facade;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DwarvenGoldDigger extends DwarvenMineWorker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DwarvenGoldDigger.class);
+
+    @Override
+    public void work() {
+        LOGGER.info("{} digs for gold.", name());
+    }
+
+    @Override
+    public String name() {
+        return "Dwarf gold digger";
+    }
+}
