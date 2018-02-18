83e61e214da435aaf9c0d5f248f6f8841a400437
Mateus <mmachado.csp@gmail.com>
2018-02-18 15:43:03 -0300

    create test

---
+++ Facade/src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import com.facade.Main;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        String[] args = {};
+        Main.main(args);
+    }
+}
