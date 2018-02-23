e56230145bd3106fb61d55fd9e731dd3d5508e59
mateusmachado <mmachado.csp@gmail.com>
2018-02-22 21:14:41 -0300

    create main class

---
+++ Decorator/src/main/java/com/decorator/App.java
@@ -0,0 +1,17 @@
+package com.decorator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("A simple looking troll approaches");
+		Troll troll = new SimpleTroll;
+		troll.attack();
+		troll.fleeBattle();
+		LOGGER.info("Clubbed troll power {}.\n", troll.getAttackPower());		
+	}
+}
